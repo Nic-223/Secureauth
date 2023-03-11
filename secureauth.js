c3403388146c6bd2f21b13c07cf259ad664c9471
Nic <69243481+Nic-223@users.noreply.github.com>
2023-03-12 00:16:28 +0100

    Update secureauth.js

--- secureauth.js
+++ secureauth.js
@@ -1,3 +1,8 @@
+--[[
+  SecureAuth, a script for FiveM servers to detect and terminate unauthorized processes that may be used for cheating or exploiting.
+  Keywords: FiveM, auth, security, anti-cheat, blacklisted apps
+]]
+
 const { spawn } = require('child_process');
 console.log('[^5SecureAuth^0] [*] Booting up...');
 
